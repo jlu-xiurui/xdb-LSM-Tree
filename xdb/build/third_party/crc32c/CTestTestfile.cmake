c91d707cf6671d4e7d7352a7d7295385e87f972f
xiurui1517 <67867150+jlu-xiurui@users.noreply.github.com>
2023-02-27 22:10:25 +0800

    Delete xdb/build directory

--- xdb/build/third_party/crc32c/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiurui/xdb/third_party/crc32c
-# Build directory: /home/xiurui/xdb/build/third_party/crc32c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
